c7b2a7941bde0dd7ac05ba91cc8211b9759e1547
Jakub Rojewski <j.rojewski95@gmail.com>
2024-02-07 19:49:49 +0100

    Day 1 - Band Name Generator

---
+++ 001/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("---BAND NAME GENERATOR---")
+
+print("Hello, so you are the one who want to start a band!")
+city = input("What was the city you were born in?\n")
+petName = input("What was the name of your first pet?\n")
+print("Generated Band name is: " + city + " " + petName)
